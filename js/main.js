41051d23182e85e05f41bf98aa067752c6df3932
Tommy Danielsson <tdserviceco@gmail.com>
2020-09-25 15:02:41 +0200

    finished mobile and Ipad

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 function openThis(event) {
   event.classList.toggle('open');
 } 
+
+function openNav() {
+  let navID = document.getElementById("main-navigation");
+  let Body = document.getElementsByTagName("BODY")[0];
+  let Header = document.getElementsByTagName("HEADER")[0];
+  navID.classList.toggle('active');
+  Body.classList.toggle('active');
+  Header.classList.toggle('active');
+}
